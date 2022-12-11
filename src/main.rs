d6cfa7cad594abc79584b9286607a98c19943295
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2022-12-11 13:44:50 +0530

    pass 3rd

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
-// Uncomment this block to pass the first stage
 use std::net::TcpListener;
+use std::net::TcpStream;
 use std::io::Write;
+use std::io::Read;
+
+
+/// helper function to handle a connection from a client
+fn handle_connection(mut stream: TcpStream) {
+    println!("Accepted new connection");
+    let mut buf = [0; 512];
+    loop {
+        let bytes_read = stream.read(&mut buf).unwrap();
+        println!("{bytes_read:?}");
+        if bytes_read == 0 {
+            println!("Client closed the connection");
+            break;
+        }
+        let _ = stream.write("+PONG\r\n".as_bytes());
+    }
+
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running
@@ -11,9 +29,8 @@ fn main() {
     let listener = TcpListener::bind("0.0.0.0:6379").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                println!("Accepted new connection");
-                _ = stream.write(b"+PONG\r\n");
+            Ok(stream) => {
+                handle_connection(stream)
             }
             Err(e) => {
                 println!("Error: {}", e);
